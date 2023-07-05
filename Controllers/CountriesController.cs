8937104ceebc551c513298e818f12e634330ff2b
r-khosravi <r.khosravi@amnpardaz.com>
2023-07-05 18:50:09 +0430

    add

--- Controllers/CountriesController.cs
+++ Controllers/CountriesController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using HotelListing.API.Data;
 using HotelListing.API.Models;
 using HotelListing.API.Dto.Country;
 using AutoMapper;
